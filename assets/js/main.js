730ebd7879a8c218f591e611c4c9370384cf57ed
Lugas Gilas Angkoro <lugasgilas@gmail.com>
2022-12-09 17:52:25 +0700

    Revisi Total
    Struktur koding lebih rapih
    Dilengkapi dengan inline documentation
    Menggunakan aturan CSS modern

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/******************************************************************
+  Template Name: Flies
+  Description:  Brand fashion Flies eCommerce frontend
+  Author: Lugas Gilas Angkoro
+  Version: 1.0.0
+  Created: PT. Aditya Mandiri Sejahtera - 2022
+******************************************************************/    
+    
+    /*------------------
+        Preloader
+    --------------------*/
+    (function ($) {
+        $(window).on('load', function () {
+            $(".loader").fadeOut();
+            $("#preloder").delay(300).fadeOut("slow");
+            });
+    })(jQuery);
+
+    
